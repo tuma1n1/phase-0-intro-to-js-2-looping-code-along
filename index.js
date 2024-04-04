98e19f4e5708c65e7b87c8eda9bb3decbe6dae68
Tumaini <tumainikebaya.234@gmail.com>
2024-04-04 17:10:31 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const eventName = 'surprise';
+
+function writeCards(name, eventName){
+    let thankYouMessages = [];
+    for(let i = 0; i < name.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+
+
+function countDown(number){
+    
+    for(let i = number; i >= 0; i--){
+        console.log(i)
+    }
+    return number;
+}
+
